7f76c2c737a04a73ec343f72cbfb69d49faf516f
Abhinav1004 <abhinavkrjha10@gmail.com>
2021-11-15 09:56:30 +0530

    added discount coupons tags in pdf invoice

--- myshop/orders/models.py
+++ myshop/orders/models.py
@@ -27,6 +27,10 @@ class Order(models.Model):
     def __str__(self):
         return f'Order {self.id}'
     
+    def get_discount(self):
+        total_cost = sum(item.get_cost() for item in self.items.all())
+        return total_cost * (self.discount / Decimal(100))
+
     def get_total_cost(self):
         total_cost = sum(item.get_cost() for item in self.items.all())
         return total_cost - total_cost * (self.discount / Decimal(100))
